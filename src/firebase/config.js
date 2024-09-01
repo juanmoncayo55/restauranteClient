e9cb6df07f7db28fa58eed24bce084aeefa1987f
juanmoncayo55 <147448954+juanmoncayo55@users.noreply.github.com>
2024-09-01 13:46:55 -0500

    eliminando keys

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,12 +1,12 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: "AIzaSyCsOIFqEDaTOCoPpPIgH-DJgktWfKZ1Kd4",
-	authDomain: "restaurant-a2398.firebaseapp.com",
-	projectId: "restaurant-a2398",
-	storageBucket: "restaurant-a2398.appspot.com",
-	messagingSenderId: "1003530903344",
-	appId: "1:1003530903344:web:f6585871359adcc1c3b2f5",
-	measurementId: "G-WRBZ8SSFHW"
+	apiKey: "...",
+	authDomain: "...",
+	projectId: "...",
+	storageBucket: "...",
+	messagingSenderId: "....",
+	appId: "....",
+	measurementId: "....."
 };
 
 export default firebaseConfig;
